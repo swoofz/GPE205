ef87090e3dac5c95a527f35643ee000e517615a8
Jordan Hall <jordanhall.1995@yahoo.com>
2019-05-21 11:45:32 -0700

    Spawning powerups Sample Code

---
+++ JHall_UATanks/Assets/Scripts/SamplePowerupCode/Pickup.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    public Powerup powerup;
+    public AudioClip feedback;
+
+    private Transform tf;
+
+    // Start is called before the first frame update
+    void Start() {
+        tf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnTriggerEnter(Collider other) {
+        // Variable to store other object's Powerup Controller - if it has one
+        PowerupController powCon = other.GetComponent<PowerupController>();
+
+        // If the other object has a Powerup Controller
+        if(powCon != null) {
+            // Add the powerup
+            powCon.Add(powerup);
+
+            // Play Feedback(if it is set)
+            if(feedback != null) {
+                AudioSource.PlayClipAtPoint(feedback, tf.position, 1.0f);
+            }
+
+            // Destory this pickup
+            Destroy(gameObject);
+        }    
+    }
+}
---
+++ JHall_UATanks/Assets/Scripts/SamplePowerupCode/PickupSpawner.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupSpawner : MonoBehaviour {
+
+    public GameObject pickupPrefab;
+    public float spawnDelay;
+
+    private float nextSpawnTime;
+    private GameObject spawnPickup;
+    private Transform tf;
+
+    // Start is called before the first frame update
+    void Start() {
+        tf = GetComponent<Transform>();
+        nextSpawnTime =  Time.time + spawnDelay;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // IF there is nothing spawns
+        if (spawnPickup == null) {
+            // Add it is time to spawn
+            if (Time.time > nextSpawnTime) {
+                // Spawn it and set the next time
+                spawnPickup = Instantiate(pickupPrefab, tf.position, Quaternion.identity) as GameObject;
+                spawnPickup.transform.parent = tf;
+                nextSpawnTime = Time.time + spawnDelay;
+            }
+        } else {
+            // Otherwise, the object still exist, so postpone the spawn
+            nextSpawnTime = Time.time + spawnDelay;
+        }
+    }
+}
