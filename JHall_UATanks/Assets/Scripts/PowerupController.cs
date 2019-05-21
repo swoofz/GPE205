40d3f46049b29ccb116a7c620bf6b468180b99f9
Jordan Hall <jordanhall.1995@yahoo.com>
2019-05-21 09:56:14 -0700

    Power ups Sample Code

--- JHall_UATanks/Assets/Scripts/PowerupController.cs
+++ JHall_UATanks/Assets/Scripts/PowerupController.cs
@@ -16,11 +16,33 @@ public class PowerupController : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        // Create an List to hold our expired powerups
+        List<Powerup> expiredPowerups = new List<Powerup>();
 
+        // Loop through all the powers in the list
+        foreach(Powerup power in powerups) {
+            // Subtract from the timer
+            power.duration -= Time.deltaTime;
+
+            // If time is up, deactivate the powerup and remove it from the list
+            if (power.duration <= 0) {
+                expiredPowerups.Add(power);
+            }
+        }
+
+        // Now that we're looked at every powerup in our list, use our list of expired powerups to remove the expired ones.
+        foreach(Powerup power in expiredPowerups) {
+            power.OnDeactivate(data);
+            powerups.Remove(power);
+        }
+
+        // Since our expiredPowerups is local, it will *poof* into nothing when this function ends
+        //  but let's clear it to learn how to empty an list
+        expiredPowerups.Clear();
     }
 
     public void Add(Powerup powerup) {
         powerup.OnActivate(data);
-        powerup.OnDeactivate(data);
+        powerups.Add(powerup);
     }
 }
