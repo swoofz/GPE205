ddf08dc61a1955e7645c534e99f4980e476668a0
Jordan Hall <jordanhall.1995@yahoo.com>
2019-05-22 09:49:48 -0700

    Map Generator Sample Code

---
+++ JHall_UATanks/Assets/Scripts/SamplePowerupCode/MapGenerator.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour {
+
+    public int rows;
+    public int cols;
+
+    private float roomWidth = 50.0f;
+    private float roomHeight = 50.0f;
+    private Room[,] grid;
+
+    public GameObject[] gridPrefabs;
+
+    // Start is called before the first frame update
+    void Start() {
+        // Generate grid
+        GenerateGrid();
+    }
+
+    public void GenerateGrid() {
+        // Clear out the grid - "which column" is our X, "which row" is our Y
+        grid = new Room[cols,rows];
+
+        // For each row...
+        for(int i = 0; i < rows; i++) {
+            // For each column in that row
+            for(int j = 0; j < cols; j++) {
+                // Figure out the location
+                float xPos = roomWidth * j;
+                float zPos = roomHeight * i;
+                Vector3 newPosition = new Vector3(xPos, 0.0f, zPos);
+
+                // Create a new Grid at the appropriate location
+                GameObject tempRoomObj = Instantiate(RandomRoomPrefab(), newPosition, Quaternion.identity) as GameObject;
+
+                // Set it parent
+                tempRoomObj.transform.parent = transform;
+
+                // Give it a meaningful name
+                tempRoomObj.name = "Room_" + j + "," + i;
+
+                // Get the Room Object
+                Room tempRoom = tempRoomObj.GetComponent<Room>();
+
+                // Open the doors
+                // If we are on the bottom row, open north door
+                if(i == 0) {
+                    tempRoom.doorNorth.SetActive(false);
+                } else if (i == rows -1) {
+                    // otherwise, if on the top row, open south door
+                    tempRoom.doorSouth.SetActive(false);
+                } else {
+                    // otherwise, we are in the middle open both doors
+                    tempRoom.doorNorth.SetActive(false);
+                    tempRoom.doorSouth.SetActive(false);
+                }
+
+                // if we are on the first column, open the east door
+                if(j == 0) {
+                    tempRoom.doorEast.SetActive(false);
+                } else if (j == cols -1) {
+                    // otherwise, if we are on the last column row, open the west door
+                    tempRoom.doorWest.SetActive(false);
+                } else {
+                    // otherwise, we are in the middle, so open both doors
+                    tempRoom.doorWest.SetActive(false);
+                    tempRoom.doorEast.SetActive(false);
+                }
+
+                // Save it to the grid
+                grid[j, i] = tempRoom;
+            }
+        }
+    }
+
+    // Return a random room
+    public GameObject RandomRoomPrefab() {
+        return gridPrefabs[Random.Range(0, gridPrefabs.Length)];
+    }
+}
